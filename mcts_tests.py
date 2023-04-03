b9be07f1ef14aa494f47e1ff8609b50c6590cd91
Daniel Lawson <daniellawson9999@gmail.com>
2023-04-03 00:41:33 -0400

    test

---
+++ mcts_tests.py
@@ -0,0 +1,35 @@
+import mcts_env as mcts
+from deck import *
+from game import *
+from player import *
+from phases import Phases
+
+from env import MtgEnv
+
+env = MtgEnv(get_8ed_core_gold_deck, 'Gold0', get_8ed_core_gold_deck, 'Gold1')
+n_games = 10
+depth = 5
+for i in range(n_games):
+    done = False
+    possible_moves, state, active_player_index = env.reset()
+    while not done:
+        #print(f"Player {active_player_index} turn")
+        #print(state)
+        #print(possible_moves)
+        # input action index
+        if len(possible_moves) == 1:
+            action = possible_moves[0]
+        else:
+            # player 0 is MCTS
+            if active_player_index == 0:
+                action = mcts.uct(env, itermax=depth)
+                if type(action) == int:
+                    action = possible_moves[action]
+                elif type(action) == tuple:
+                    action = action[0]
+                #import pdb; pdb.set_trace()
+            else:
+                # random action
+                action = random.choice(possible_moves)
+        state, possible_moves, active_player_index, done, info = env.step(action)
+    print(f"Player {info['winning_player']} wins")
