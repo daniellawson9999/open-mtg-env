b3ba7b9880087e5e00e95c53226db246e62f2674
Daniel Lawson <daniellawson9999@gmail.com>
2023-04-06 13:23:02 -0400

    changes

--- lm_utils.py
+++ lm_utils.py
@@ -2,6 +2,8 @@ import random
 
 import torch
 import numpy as np
+from open_mtg_env import mcts_env as mcts
+
 
 # Utilities for lm
 
@@ -9,6 +11,8 @@ def score_response(model, tokenizer, query, response, device):
     # encode a query
     input_txt = query + response
     input_tensor = tokenizer.encode(input_txt, return_tensors="pt").to(device)
+    # TODO, update to only score after query
+    raise Exception('update')
 
     # get model output, and then logits
     output = model(input_tensor)
@@ -32,24 +36,60 @@ def get_action_lm(env, model, tokenizer, state, possible_moves, device, sample_a
         predicted_action = np.random.choice(possible_moves, p=torch.softmax(torch.tensor(scores),dim=-1).numpy())
     return predicted_action
 
-def get_action(env, model, tokenizer, state, possible_moves, mode, device, sample_action=False):
+def get_action_lm_batched(env, model, tokenizer, state, possible_moves, device, sample_action=False):
+    # batched version of get_action_lm, with only 1 forward pass
+    input_txts  = []
+    query = env.state_action_to_query(state, possible_moves)
+    for action in possible_moves:
+        response = env.format_action(action)
+        input_txts.append(query + response)
+    # tokenize input txt
+    input_tensors = tokenizer(input_txts, return_tensors="pt", padding=True).to(device)
+    #input_tensors = tokenizer(input_txts, padding=True)
+    # get model output
+    output = model(**input_tensors)
+    # get logits
+    logits = output[0]
+    scores = []
+    # add up logits for each action
+    for i in range(len(possible_moves)):
+        # make sure to exclude padding tokens
+        input_tensor = input_tensors['input_ids'][i]
+        score = 0
+        for j in range(len(input_tensor)):
+            if input_tensor[j] != tokenizer.pad_token_id:
+                score += logits[i][j][input_tensor[j]].item()
+        scores.append(score)
+    # get action with highest score
+    action = possible_moves[np.argmax(scores)]
+    return action
+
+def get_action(env, model, tokenizer, state, possible_moves, mode, device, sample_action=False, depth=5):
     if len(possible_moves) == 1:
             action = possible_moves[0] 
     elif mode == 'random':
         action = random.sample(range(len(possible_moves)), 1)[0]
         action = possible_moves[action]
     elif mode == 'lm':
+        # originally get_action_lm
         action = get_action_lm(env, model, tokenizer, state, possible_moves, device, sample_action)
+        #action = get_action_lm_batched(env, model, tokenizer, state, possible_moves, device, sample_action)
     elif mode == 'human':
         action_index = input("Enter action: ")
         action = possible_moves[int(action_index)]
+    elif mode == 'mcts':
+        action = mcts.uct(env, itermax=depth)
+        if type(action) == int:
+            action = possible_moves[action]
+        elif type(action) == tuple:
+            action = action[0]
     else:
         raise Exception("invalid model type")
     return action
 
 
-def run_games(env, model, tokenizer, device, n_games=1, mode_0='random', mode_1='random', lm_sample=False, print_state=False):
-    assert mode_0 in ['random', 'lm', 'human'] and mode_1 in ['random', 'lm', 'human'], "invalid model type"
+def run_games(env, model, tokenizer, device, n_games=1, mode_0='random', mode_1='random', lm_sample=False, print_state=False, depth=5):
+    assert mode_0 in ['random', 'lm', 'human', 'mcts'] and mode_1 in ['random', 'lm', 'human', 'mcts'], "invalid model type"
     modes = [mode_0, mode_1]
 
     queries = []
@@ -72,7 +112,7 @@ def run_games(env, model, tokenizer, device, n_games=1, mode_0='random', mode_1=
             if print_state:
                 print(query_str)
 
-            action = get_action(env, model, tokenizer, state, possible_moves, modes[active_player_index], device, sample_action=lm_sample)
+            action = get_action(env, model, tokenizer, state, possible_moves, modes[active_player_index], device, sample_action=lm_sample, depth=depth)
 
             # Store transition
             if len(possible_moves) > 1:
--- supervised_lm.py
+++ supervised_lm.py
@@ -1,12 +1,14 @@
+import random
+import os
+import pickle
+import copy
+import argparse
+
 from open_mtg_env.deck import *
 from open_mtg_env.game import *
 from open_mtg_env.player import *
 from open_mtg_env.phases import Phases
-import copy
 from open_mtg_env.env import MtgEnv
-import random
-import os
-import pickle
 
 from dataclasses import dataclass, field
 from typing import Optional
@@ -19,6 +21,8 @@ from transformers import AutoTokenizer, HfArgumentParser, pipeline, Trainer, Tra
 
 #from trl import AutoModelForCausalLM, set_seed
 from trl.core import LengthSampler, respond_to_batch
+
+from lm_utils import run_games
 
 def print_trainable_parameters(model):
     """
@@ -35,19 +39,24 @@ def print_trainable_parameters(model):
     )
 
 class MtgDataset(torch.utils.data.Dataset):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
     def __getitem__(self, idx):
-        return {'text': self.data[idx]}
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        #item['labels'] = item['input_ids'].clone()
+        return item
     def __len__(self):
-        return len(self.data)
+        return len(self.encodings)
 
 def main(args):
     model = AutoModelForCausalLM.from_pretrained(
         args.model_name,
         #trust_remote_code=True,
         #use_cache=not args.no_gradient_checkpointing,
-        #load_in_8bit=True,
+        load_in_8bit=True,
+        device_map='auto',
         #device_map={"": Accelerator().process_index},
     )
    # model = prepare_model_for_int8_training(model)
@@ -68,39 +77,63 @@ def main(args):
     print_trainable_parameters(model)
 
     # prepare custom dataset
-    data_name = 'data_mcts_random_g3_d8_34386'
-    with open(os.path.join('data', data_name, 'rb')) as f:
+    data_name = 'data_mcts_2_g250_d10_74432' + '.pkl'
+    with open(os.path.join('data', data_name),'rb') as f:
         data = pickle.load(f)
     
-    env = MtgEnv() # TODO
+    env = MtgEnv(get_8ed_core_gold_deck, 'Gold0', get_8ed_core_gold_deck, 'Gold1') # TODO
 
     input_strs = []
     # iterate over trajectories
+    max_traj_len = -1
+    input_lens = []
+    n_trajectories = 0
     for i in range(len(data['states'])):
         # check if winning trajectory
         if data['rewards'][i][0] != 1:
             continue
-        for j in range(len(data['states'][i])):
+        else:
+            n_trajectories += 1
+            
+        traj_len = len(data['states'][i])
+        if traj_len > max_traj_len:
+            max_traj_len = traj_len
+
+        for j in range(traj_len):
             state = data['states'][i][j]
-            possible_moves = data['possible_moves'][i][j]
+            possible_moves = data['legal_actions'][i][j]
             action = data['actions'][i][j]
             query = env.state_action_to_query(state, possible_moves)
             response = env.format_action(action)
             input_str = query + response
+            input_len = len(input_str)
+            input_lens.append(input_len)
             input_strs.append(input_str)
     # create dataset
-    train_dataset = MtgDataset(input_strs)
+    print(f'Creating dataset with {n_trajectories}, max trajectory len {max_traj_len}, max input len {max(input_lens)}, mean input len: {np.mean(input_lens)}')
 
-    # tokenize, TODO, could change padding, etc
-    train_dataset = train_dataset.map(lambda x: tokenizer(x['text'], padding='max_length', truncation=True), batched=True)
+    input_encodings = tokenizer(input_strs, padding=True, return_offsets_mapping=True) #truncation=True, maybe adjust
+    labels = []
+    for i in range(len(input_encodings['input_ids'])):
+        offset_map = input_encodings['offset_mapping'][i]
+        first_index, last_index = zip(*offset_map)
+        last_index = np.array(last_index)
+        target_str = "best:" # make sure this is the same as in env
+        # TODO
+        # get start of response
+        #response_index = input_strs[i].index(target_str)
+        start_of_output = input_strs[0].index(target_str) + len(target_str)
+        np_ids = input_encodings['input_ids'][i]
+        label = np.where(last_index < start_of_output,-100,np_ids)
+        labels.append(label)
+
+    train_dataset = MtgDataset(input_encodings, labels)
+
+    #import pdb; pdb.set_trace()
 
     training_args = TrainingArguments(
         output_dir=args.output_dir,
-        dataloader_drop_last=True,
-        evaluation_strategy="steps",
         max_steps=args.max_steps,
-        eval_steps=args.eval_freq,
-        save_steps=args.save_freq,
         logging_steps=args.log_freq,
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
@@ -108,12 +141,10 @@ def main(args):
         lr_scheduler_type=args.lr_scheduler_type,
         warmup_steps=args.num_warmup_steps,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
-        gradient_checkpointing=not args.no_gradient_checkpointing,
-        fp16=not args.no_fp16,
-        bf16=args.bf16,
         weight_decay=args.weight_decay,
-        run_name="llama-7b-finetuned",
-        report_to="wandb",
+        #run_name="gpt-test",
+        #report_to="wandb",
+        report_to= "none",
         ddp_find_unused_parameters=False,
     )
     # create trainer
@@ -121,28 +152,64 @@ def main(args):
 
     trainer.train()
 
+    # eval model
+    queries, actions, rewards, player_0_wins, player_1_wins = run_games(env, model, tokenizer, args.device, n_games=args.eval_games, mode_0='lm', mode_1='mcts', lm_sample=False, print_state=False, depth=args.depth)
+    print(f"LM vs MCTS depth {args.depth}")
+    print("Player 0 wins: ", player_0_wins)
+    print("Player 1 wins: ", player_1_wins)
+
+    queries, actions, rewards, player_0_wins, player_1_wins = run_games(env, model, tokenizer, args.device, n_games=args.eval_games, mode_0='lm', mode_1='random', lm_sample=False, print_state=False, depth=args.depth)
+    print("LM vs Random")
+    print("Player 0 wins: ", player_0_wins)
+    print("Player 1 wins: ", player_1_wins)
+
+    #import pdb; pdb.set_trace()
+
 
 if __name__ == '__main__':
-    @dataclass
-    class ScriptArguments:
-        """
-        The name of the Casual LM model we wish to fine with PPO
-        """
+    # @dataclass
+    # class ScriptArguments:
+    #     """
+    #     The name of the Casual LM model we wish to fine with PPO
+    #     """
 
-        # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
-        # models like gpt-neo* models are more suitable.
-        #model_name: Optional[str] = field(default="edbeeching/gpt-neo-125M-imdb", metadata={"help": "the model name"})
-        model_name: Optional[str] = field(default="EleutherAI/gpt-neo-125M", metadata={"help": "the model name"})
-        #model_name: Optional[str] = field(default="EleutherAI/gpt-neo-1.3B", metadata={"help": "the model name"})
-        log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
-        learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "the learning rate"})
-        mini_batch_size: Optional[int] = field(default=4, metadata={"help": "the PPO minibatch size"})
-        batch_size: Optional[int] = field(default=64, metadata={"help": "the batch size"}) # 256
-        gradient_accumulation_steps: Optional[int] = field(
-            default=1, metadata={"help": "the number of gradient accumulation steps"}
-        )
+    #     # NOTE: gpt2 models use Conv1D instead of Linear layers which are not yet supported in 8 bit mode
+    #     # models like gpt-neo* models are more suitable.
+    #     #model_name: Optional[str] = field(default="edbeeching/gpt-neo-125M-imdb", metadata={"help": "the model name"})
+    #     model_name: Optional[str] = field(default="EleutherAI/gpt-neo-125M", metadata={"help": "the model name"})
+    #     #model_name: Optional[str] = field(default="EleutherAI/gpt-neo-1.3B", metadata={"help": "the model name"})
+    #     log_with: Optional[str] = field(default=None, metadata={"help": "use 'wandb' to log with wandb"})
+    #     learning_rate: Optional[float] = field(default=1.41e-5, metadata={"help": "the learning rate"})
+    #     mini_batch_size: Optional[int] = field(default=4, metadata={"help": "the PPO minibatch size"})
+    #     batch_size: Optional[int] = field(default=64, metadata={"help": "the batch size"}) # 256
+    #     gradient_accumulation_steps: Optional[int] = field(
+    #         default=1, metadata={"help": "the number of gradient accumulation steps"}
+    #     )
 
-    parser = HfArgumentParser(ScriptArguments)
-    script_args = parser.parse_args_into_dataclasses()[0]
+    # parser = HfArgumentParser(ScriptArguments)
+    # script_args = parser.parse_args_into_dataclasses()[0]
 
-    main(script_args)
+    # instead use argparse
+    parser = argparse.ArgumentParser()
+    #parser.add_argument('--model_name', type=str, default='EleutherAI/gpt-neo-125M')
+    parser.add_argument('--device', type=str, default='cuda:0'),
+    parser.add_argument('--model_name', type=str, default='EleutherAI/Pythia-410m') #1b, 410m worked..Pythia-410m
+    #parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
+
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
+    parser.add_argument("--max_steps", type=int, default=1000)
+    parser.add_argument("--num_warmup_steps", type=int, default=100)
+    parser.add_argument("--weight_decay", type=float, default=0.05)
+    parser.add_argument("--log_freq", default=1, type=int)
+    parser.add_argument("--output_dir", default="./checkpoints", type=str)
+
+    parser.add_argument("--eval_games", default='5', type=int)
+    parser.add_argument('--depth', type=int, default=5)
+
+    args = parser.parse_args()
+    # TODO mask idea, use this, return_offsets_mapping=True
+    # https://stackoverflow.com/questions/63413414/is-there-a-way-to-get-the-location-of-the-substring-from-which-a-certain-token-h
+    main(args)
